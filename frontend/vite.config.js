fc4738cb29b84f676877231937e49ce9be4bf40d
a.blech <a.blech@profeelab.net>
2025-09-16 10:18:45 +0200

    fix: added domain to allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,4 +15,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: true,
+    port: 5173,
+    // Add your production domain here to allow it
+    // The host "simple-expense-tracker.de" is blocked by default.
+    allowedHosts: [
+      'simple-expense-tracker.de',
+      'www.simple-expense-tracker.de'
+    ]
+  }
 })
